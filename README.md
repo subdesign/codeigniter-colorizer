7944d7c358e469a9e595987f9b5083c12b9003af
subdesign <info@subdesign.hu>
2011-09-30 12:17:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Codeigniter Colorizer
+
+Simply fun library for colorizing text input. This is a raw version, further functionalities planned.
+
+---
+
+## Usage
+
+First, put the Ci_colorizer.php into you application/libraries folder
+
+Second, load the library:
+
+    $this->load->library('ci_colorizer');
+
+Then call the library colorize methor with your text string parameter:
+
+    $this->ci_colorizer->colorize('..your text here..');
+
+If you want to specify what colors do you want to use with, give a 2nd parameter (offical HTML color names needed):
+
+    $this->ci_colorizer->colorize('..your text here..', 'Green, Blue, Maroon');
+
+List of offical [HTML color names]{http://www.w3schools.com/html/html_colornames.asp} 
+
+## Notes
+
+*    You can extend the $constant_colos array with more HTML color names
---
+++ libraries/ci_colorizer.php
@@ -0,0 +1,52 @@
+<?php 
+
+/**
+ *  Colorizer
+ *
+ *  Colorizer is a text colorizer library
+ *
+ *  @version 0.1
+ *	@author Barna Szalai <sz.b@subdesign.hu>
+ *  @param string $text input text
+ *  $param string $colors optional colors  
+ *  @return string
+ */
+
+class Ci_colorizer
+{
+	private $text;
+	private $colors = array();
+	private $constant_colors = array('Aqua', 'Black', 'Blue', 'Brown', 'Cyan', 'Gold', 'Gray', 'Green', 'Lime', 'Magenta', 'Navy', 'Orange', 'Orchid', 'Purple', 'Red', 'Silver', 'Violet', 'Yellow');
+
+	public function colorize($text, $colors = '')
+	{
+		$this->text = $text;
+		
+		$text_array = explode(" ", $this->text);
+		
+		if(strlen($colors)) 
+		{
+			$this->colors = $colors;
+			$this->colors = explode(", ", $this->colors);
+		}
+
+		$result = '';
+		
+		if(count($this->colors) > 0)
+		{
+			for($i = 0; $i < count($text_array); $i++)
+			{
+				$result .= '<span style="color:'.$this->colors[array_rand($this->colors, 1)].'">'.$text_array[$i].' </span>';
+			}
+		}
+		else
+		{
+			for($i = 0; $i < count($text_array); $i++)
+			{
+				$result .= '<span style="color:'.$this->constant_colors[array_rand($this->constant_colors, 1)].'">'.$text_array[$i].' </span>';
+			}
+		}
+		
+		return $result;	
+	}	
+}
